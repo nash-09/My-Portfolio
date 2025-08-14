205bb1d271470047237bc4593a7f842cd92c7725
Nash-09 <niranjanbarupal182@gmail.com>
2025-08-15 01:58:11 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-project/",
+  base: "/My-Portfolio/",
   plugins: [react()],
 })
