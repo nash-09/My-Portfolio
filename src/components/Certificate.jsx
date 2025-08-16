39a86f4687e3e66e5f6a352df7b3421b40bd3166
Niranjan Barupal <niranjanbarupal182@gmail.com>
2025-08-16 17:38:50 +0530

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -7,14 +7,14 @@ const Certificate = () => {
         <h3 className='text-xl font-semibold text-slate-700 mt-3 mb-3'>RSCIT Certificate</h3>
         <p className='mb-3'>Vardhman Mahaveer Open University, Kota (March 2021)</p>
         <p>Completed the Rajasthan State Certificate in Information Technology (RSCIT) with 80% marks. This certification provided me with a strong foundation in computer fundamentals, IT applications, and internet usage.</p>
-        <p className='text-sky-600 font-semibold inline-block mb-3 mt-3'>Verification:</p> <a href="/Certificates/RSCIT Certificate.jpg" target='_blank' rel='noopener noreferrer' className='text-sky-500 hover:text-sky-700'>View Certificate</a>
+        <p className='text-sky-600 font-semibold inline-block mb-3 mt-3'>Verification:</p> <a href="Certificates/RSCITCertificate.jpg" target='_blank' rel='noopener noreferrer' className='text-sky-500 hover:text-sky-700'>View Certificate</a>
         
         <div className='w-full border-2 border-dashed border-sky-700 mt-5 mb-5'></div>
 
         <h3 className='text-xl text-slate-700 font-semibold mt-5 mb-3'>Advanced Basic Computer Certificate</h3>
         <p className='mb-3'>KRS Academy, Beawar (April 2022)</p>
         <p>Successfully completed a certified course in Advanced Basic Computer and was awarded Grade A+ for excellent performance. The course enhanced my practical knowledge of essential computer operations and applications.</p>
-        <p className='text-sky-600 font-semibold inline-block mb-3 mt-3'>Verification:</p> <a href="/Certificates/Advanced Basic Computer Certificate.jpg" target='_blank' rel='noopener noreferrer' className='text-sky-500 hover:text-sky-700'>View Certificate</a>
+        <p className='text-sky-600 font-semibold inline-block mb-3 mt-3'>Verification:</p> <a href="Certificates/BasicComputer.jpg" target='_blank' rel='noopener noreferrer' className='text-sky-500 hover:text-sky-700'>View Certificate</a>
 
         <div className='w-full border-2 border-dashed border-sky-700 mt-5 mb-5'></div>
 
